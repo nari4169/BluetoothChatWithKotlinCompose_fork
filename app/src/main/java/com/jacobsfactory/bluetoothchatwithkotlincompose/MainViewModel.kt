1e82b481abd71fa512b358ee8f4469910fde4dee
jacob <jacobchoi77@gmail.com>
2022-05-17 23:54:17 +0900

    refactoring

--- app/src/main/java/com/jacobsfactory/bluetoothchatwithkotlincompose/MainViewModel.kt
+++ app/src/main/java/com/jacobsfactory/bluetoothchatwithkotlincompose/MainViewModel.kt
@@ -17,14 +17,6 @@ import java.util.*
 class MainViewModel(application: Application) : AndroidViewModel(application) {
     val mBluetoothAdapter: BluetoothAdapter by lazy { BluetoothAdapter.getDefaultAdapter() }
     var connectionState by mutableStateOf(ConnectionState.STATE_PERMISSION)
-    var mConnectedDeviceName = mutableStateOf("")
-
-    val TAG = "BluetoothChatService"
-    private val NAME_SECURE = "BluetoothChatSecure"
-    private val MY_UUID_SECURE = UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a66")
-
-    val conversations = mutableStateListOf<String>()
-
     val stateString = derivedStateOf {
         when (connectionState) {
             ConnectionState.STATE_NONE -> application.getString(R.string.title_not_connected)
@@ -32,32 +24,31 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
             ConnectionState.STATE_LISTEN -> application.getString(R.string.title_listening)
             ConnectionState.STATE_CONNECTED -> application.getString(
                 R.string.title_connected_to,
-                mConnectedDeviceName.value
+                mConnectedDeviceName
             )
             else -> ""
         }
     }
+    var mConnectedDeviceName by mutableStateOf("")
+    val conversations = mutableStateListOf<String>()
 
-    private var mSecureAcceptThread: AcceptThread? = null
+    companion object {
+        val TAG = "BluetoothChatService"
+        private val NAME_SECURE = "BluetoothChatSecure"
+        private val MY_UUID_SECURE = UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a66")
+    }
+
+    private var mAcceptThread: AcceptThread? = null
     private var mConnectThread: ConnectThread? = null
     private var mConnectedThread: ConnectedThread? = null
 
     @Synchronized
     fun start() {
         Log.d(TAG, "start")
-        if (mConnectThread != null) {
-            mConnectThread!!.cancel()
-            mConnectThread = null
-        }
-
-        if (mConnectedThread != null) {
-            mConnectedThread!!.cancel()
-            mConnectedThread = null
-        }
-
-        if (mSecureAcceptThread == null) {
-            mSecureAcceptThread = AcceptThread(true)
-            mSecureAcceptThread!!.start()
+        cleanConnectThread()
+        cleanConnectedThread()
+        if (mAcceptThread == null) {
+            mAcceptThread = AcceptThread().apply { start() }
         }
     }
 
@@ -66,56 +57,29 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
         val device = mBluetoothAdapter.getRemoteDevice(address)
         Log.d(TAG, "connect to: $device")
         if (connectionState == ConnectionState.STATE_CONNECTING) {
-            if (mConnectThread != null) {
-                mConnectThread!!.cancel()
-                mConnectThread = null
-            }
+            cleanConnectThread()
         }
-        if (mConnectedThread != null) {
-            mConnectedThread!!.cancel()
-            mConnectedThread = null
-        }
-        mConnectThread = ConnectThread(device)
-        mConnectThread!!.start()
+        cleanConnectedThread()
+        mConnectThread = ConnectThread(device).apply { start() }
     }
 
     @SuppressLint("MissingPermission")
     @Synchronized
-    fun connected(socket: BluetoothSocket?, device: BluetoothDevice) {
+    fun connected(socket: BluetoothSocket, device: BluetoothDevice) {
         Log.d(TAG, "connected")
-        if (mConnectThread != null) {
-            mConnectThread!!.cancel()
-            mConnectThread = null
-        }
-        if (mConnectedThread != null) {
-            mConnectedThread!!.cancel()
-            mConnectedThread = null
-        }
-        if (mSecureAcceptThread != null) {
-            mSecureAcceptThread!!.cancel()
-            mSecureAcceptThread = null
-        }
-        mConnectedThread = ConnectedThread(socket!!)
-        mConnectedThread!!.start()
-
-        mConnectedDeviceName.value = device.name
+        cleanConnectThread()
+        cleanConnectedThread()
+        cleanAcceptedThread()
+        mConnectedThread = ConnectedThread(socket).apply { start() }
+        mConnectedDeviceName = device.name
     }
 
     @Synchronized
     fun stop() {
         Log.d(TAG, "stop")
-        if (mConnectThread != null) {
-            mConnectThread!!.cancel()
-            mConnectThread = null
-        }
-        if (mConnectedThread != null) {
-            mConnectedThread!!.cancel()
-            mConnectedThread = null
-        }
-        if (mSecureAcceptThread != null) {
-            mSecureAcceptThread!!.cancel()
-            mSecureAcceptThread = null
-        }
+        cleanConnectThread()
+        cleanConnectedThread()
+        cleanAcceptedThread()
         connectionState = ConnectionState.STATE_NONE
     }
 
@@ -137,7 +101,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
     }
 
     @SuppressLint("MissingPermission")
-    private inner class AcceptThread(secure: Boolean) : Thread() {
+    private inner class AcceptThread : Thread() {
         private val mmServerSocket: BluetoothServerSocket? by lazy(LazyThreadSafetyMode.NONE) {
             mBluetoothAdapter.listenUsingInsecureRfcommWithServiceRecord(
                 NAME_SECURE,
@@ -150,7 +114,6 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
         }
 
         override fun run() {
-            // Keep listening until exception occurs or a socket is returned.
             while (connectionState != ConnectionState.STATE_CONNECTED) {
                 val socket: BluetoothSocket? = try {
                     mmServerSocket?.accept()
@@ -158,15 +121,13 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
                     Log.e(TAG, "Socket's accept() method failed", e)
                     break
                 }
-                socket?.also {
+                socket?.let {
                     when (connectionState) {
                         ConnectionState.STATE_LISTEN, ConnectionState.STATE_CONNECTING ->
-                            connected(
-                                socket, socket.remoteDevice
-                            )
+                            connected(it, it.remoteDevice)
                         ConnectionState.STATE_NONE, ConnectionState.STATE_CONNECTED ->                                 // Either not ready or already connected. Terminate new socket.
                             try {
-                                socket.close()
+                                it.close()
                             } catch (e: IOException) {
                                 Log.e(TAG, "Could not close unwanted socket", e)
                             }
@@ -208,17 +169,13 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
                     try {
                         socket.close()
                     } catch (e2: IOException) {
-                        Log.e(
-                            TAG,
-                            "unable to close() socket during connection failure",
-                            e2
-                        )
+                        Log.e(TAG, "unable to close() socket during connection failure", e2)
                     }
                     connectionFailed()
                     return
                 }
                 synchronized(this) { mConnectThread = null }
-                connected(mmSocket, mmDevice)
+                connected(socket, mmDevice)
             }
         }
 
@@ -249,7 +206,7 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
                 try {
                     bytes = mmInStream.read(mmBuffer)
                     val readMessage = String(mmBuffer, 0, bytes)
-                    conversations.add("${mConnectedDeviceName.value}  :    $readMessage")
+                    conversations.add("$mConnectedDeviceName  :    $readMessage")
                 } catch (e: IOException) {
                     Log.e(TAG, "disconnected", e)
                     connectionLost()
@@ -276,6 +233,27 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
             } catch (e: IOException) {
                 Log.e(TAG, "Could not close the connect socket", e)
             }
+        }
+    }
+
+    private fun cleanAcceptedThread() {
+        mAcceptThread?.let {
+            it.cancel()
+            mAcceptThread = null
+        }
+    }
+
+    private fun cleanConnectedThread() {
+        mConnectedThread?.let {
+            it.cancel()
+            mConnectedThread = null
+        }
+    }
+
+    private fun cleanConnectThread() {
+        mConnectThread?.let {
+            it.cancel()
+            mConnectThread = null
         }
     }
 }
--- app/src/main/java/com/jacobsfactory/bluetoothchatwithkotlincompose/utils.kt
+++ app/src/main/java/com/jacobsfactory/bluetoothchatwithkotlincompose/utils.kt
@@ -59,3 +59,4 @@ fun FeatureThatRequiresPermissions(viewModel: MainViewModel, content: @Composabl
     }
 }
 
+
